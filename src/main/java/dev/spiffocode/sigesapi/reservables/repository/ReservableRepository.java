81db11fe29ea9dbdcd3369e3693467889f26d1e8
YHRFNTS <yahirfuentes2006@gmail.com>
2026-02-14 15:57:22 -0600

    feat: add JPA repositories for domain entities

---
+++ src/main/java/dev/spiffocode/sigesapi/reservables/repository/ReservableRepository.java
@@ -0,0 +1,16 @@
+package dev.spiffocode.sigesapi.reservables.repository;
+
+import dev.spiffocode.sigesapi.reservables.model.Reservable;
+import dev.spiffocode.sigesapi.reservables.model.ReservableStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReservableRepository extends JpaRepository<Reservable, Long> {
+
+    List<Reservable> findByBuildingId(Long buildingId);
+
+    List<Reservable> findByStatus(ReservableStatus status);
+}
