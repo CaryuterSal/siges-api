be020728f5f9f6a5ffaa9d5df4b163cf1c294dbc
CaryuterSal <20243ds158@utez.edu.mx>
2026-02-28 06:23:44 -0600

    chore: add mock mail propeties for test

--- src/main/java/dev/spiffocode/sigesapi/reservables/presentation/dto/ReservableRegisterDto.java
+++ src/main/java/dev/spiffocode/sigesapi/reservables/presentation/dto/ReservableRegisterDto.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
 import lombok.experimental.NonFinal;
@@ -13,6 +14,7 @@ import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
+@AllArgsConstructor
 @SuperBuilder(toBuilder = true)
 @Value
 @NonFinal
